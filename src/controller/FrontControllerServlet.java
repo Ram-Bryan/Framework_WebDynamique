27e641f6b630466b189f1a09f07d4504f67c82bf
Bryan <ramboliarisoabryan1@gmail.com>
2026-06-12 10:35:20 +0300

    Ajout foncton processRequest

--- src/controller/FrontControllerServlet.java
+++ src/controller/FrontControllerServlet.java
@@ -8,26 +8,24 @@ import java.io.PrintWriter;
 
 public class FrontControllerServlet extends HttpServlet {
 
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                        throws ServletException, IOException {
+        private void processRequest(HttpServletRequest request,  HttpServletResponse response) throws ServletException, IOException {
                 response.setContentType("text/html");
                 PrintWriter out = response.getWriter();
                 String url = request.getRequestURL().toString();
-
                 out.println(url);
-                
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                processRequest(request, response);
         }
 
         @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response)
                         throws ServletException, IOException {
 
-                response.setContentType("text/html");
-                PrintWriter out = response.getWriter();
-                String url = request.getRequestURL().toString();
-
-                out.println(url);
+                processRequest(request, response);
 
         }
 
